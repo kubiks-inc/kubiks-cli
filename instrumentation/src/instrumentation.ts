42f6966291d1c3ecddcd3a69dac63e2ca085f477
Alex Holovach <alex.holovach@kubiks.ai>
2025-08-15 13:53:42 -0500

    update

--- instrumentation/src/instrumentation.ts
+++ instrumentation/src/instrumentation.ts
@@ -1,7 +1,8 @@
-const { KubiksSDK } = require('@kubiks/otel-nextjs');
+const { registerOTel, OTLPHttpJsonTraceExporter } = require('@vercel/otel');
 
-// The service name will be automatically set from OTEL_SERVICE_NAME environment variable
-// which is set by kubiks-cli when starting the Next.js app
-const sdk = new KubiksSDK();
-
-sdk.start();
+registerOTel({
+  serviceName: 'kubiks-cli',
+  traceExporter: new OTLPHttpJsonTraceExporter({
+    url: 'http://localhost:7432/v1/traces',
+  }),
+});
